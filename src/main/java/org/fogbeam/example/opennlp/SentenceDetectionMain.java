91f7cbc618513b63c18bb0a3979f287743e9f70b
mikelmujika16 <124912823+mikelmujika16@users.noreply.github.com>
2024-11-20 23:41:01 +0000

    reliability

--- src/main/java/org/fogbeam/example/opennlp/SentenceDetectionMain.java
+++ src/main/java/org/fogbeam/example/opennlp/SentenceDetectionMain.java
@@ -14,13 +14,14 @@ public class SentenceDetectionMain
 {
 	public static void main( String[] args ) throws Exception
 	{
-		InputStream modelIn = new FileInputStream( "models/en-sent.model" );
-		InputStream demoDataIn = new FileInputStream( "demo_data/en-sent1.demo" );
 		
 		
 		
-		try
+		
+		try (InputStream modelIn = new FileInputStream( "models/en-sent.model" );
+		InputStream demoDataIn = new FileInputStream( "demo_data/en-sent1.demo" ))
 		{
+			
 			SentenceModel model = new SentenceModel( modelIn );
 			SentenceDetectorME sentenceDetector = new SentenceDetectorME(model);
 			
@@ -28,6 +29,8 @@ public class SentenceDetectionMain
 			
 			String[] sentences = sentenceDetector.sentDetect( demoData );
 			
+			modelIn.close();
+			demoDataIn.close();
 			for( String sentence : sentences )
 			{
 				System.out.println( sentence + "\n" );
@@ -39,34 +42,9 @@ public class SentenceDetectionMain
 		catch( IOException e )
 		{
 		}
-		finally
-		{
-				try
-				{
-					modelIn.close();
-				}
-				catch( IOException e )
-				{
-				}
-			}
-
-				try
-				{
-					demoDataIn.close();
-				}
-				catch( IOException e )
-				{
-				}
-
-			
-
 		
-		
-		System.out.println( "done" );
-		
+	
 	}
-	
-	
 	static String convertStreamToString(java.io.InputStream is) {
 	    java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
 	    return s.hasNext() ? s.next() : "";
